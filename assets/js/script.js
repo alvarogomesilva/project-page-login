268cd5c54d0b6e92d7291a97f003ea6e67ab688a
Alvaro Henrique <alvaroindaial15@gmail.com>
2024-11-02 09:42:58 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+function togglePassword() {
+    document
+        .querySelectorAll(".eye")
+        .forEach((eye) => eye.classList.toggle("hide"))
+
+    const type = password.getAttribute("type") == "password" ? "text" : "password"
+    password.setAttribute("type", type)
+}
